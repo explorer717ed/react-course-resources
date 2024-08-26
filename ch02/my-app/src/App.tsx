71c4775c6d9759019f05bd981b7f44e541881d8a
717 <explorer717ed@gmail.com>
2024-08-26 14:31:09 +1200

    feat: ch3-1, class component

--- ch02/my-app/src/App.tsx
+++ ch02/my-app/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <h1>haha</h1>
+//   );
+// }
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>hehe</h1>
+  )
 }
 
 export default App;
---
+++ ch02/my-app/src/App2.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+// class is an old way of making component
+class App2 extends React.Component {
+
+  render(): React.ReactNode {
+    return <h1>ohhhh classss</h1>
+  }
+}
+
+export default App2
